f9fb97edc5e6f396a0b2075a1a93ba706899872e
Hashblen <hashanulblennon@gmail.com>
2022-03-08 23:30:54 +0100

    support for 1.8-1.12 because code doesn't change

--- src/main/java/com/github/hashblen/TASPlace.java
+++ src/main/java/com/github/hashblen/TASPlace.java
@@ -4,21 +4,20 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
-@Mod(modid = TASPlace.MODID, version = TASPlace.VERSION)
+
+@Mod(modid = TASPlace.MODID, version = TASPlace.VERSION, acceptedMinecraftVersions = "[1.8,1.12.2]")
 public class TASPlace
 {
     public static final String MODID = "tasplace";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
