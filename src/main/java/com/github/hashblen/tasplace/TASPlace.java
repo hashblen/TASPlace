5ceb031d9bea121201402f5f1cd841706a9b82fe
Hashblen <hashanulblennon@gmail.com>
2022-03-13 09:50:43 +0100

    now supports all keybinds

--- src/main/java/com/github/hashblen/tasplace/TASPlace.java
+++ src/main/java/com/github/hashblen/tasplace/TASPlace.java
@@ -1,4 +1,4 @@
-package com.github.hashblen;
+package com.github.hashblen.tasplace;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.GameSettings;
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
 
@@ -16,7 +17,7 @@ import org.lwjgl.input.Mouse;
 public class TASPlace
 {
     public static final String MODID = "tasplace";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
@@ -29,9 +30,17 @@ public class TASPlace
         Minecraft minecraft = Minecraft.getMinecraft();
         if(event.phase!= TickEvent.Phase.START || !minecraft.isSingleplayer() || minecraft.currentScreen!=null) return;
         GameSettings gameSettings = minecraft.gameSettings;
-        if(Mouse.isButtonDown(1)){
+        int keyCode = gameSettings.keyBindUseItem.getKeyCode();
+        if((keyCode>=-100 && keyCode<=-85) && Mouse.isButtonDown(keyCode+100)){ //mouse button 0 (left click) to button 15 (max)
             //KeyBinding.setKeyBindState(gameSettings.keyBindUseItem.getKeyCode(), true);
-            KeyBinding.onTick(gameSettings.keyBindUseItem.getKeyCode());
+            KeyBinding.onTick(keyCode);
+            return;
         }
+        if(Keyboard.isKeyDown(keyCode)){
+            //System.out.println(keyCode);
+            KeyBinding.onTick(keyCode);
+        }
+
+        //System.out.println(Keyboard.getKeyName(keyCode));
     }
 }
